9c5ad088861da937558f40be62e59716061562fb
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-07-19 13:30:38 -0500

    Creacion de data de registros Dummy

--- pagos/src/app/app.component.ts
+++ pagos/src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pagos Masivos';
+  num_lote = 1;
+  registros = [
+    {
+      "num_lote": 1,
+      "nom_archivo": "Centro CC Los Molinos",
+      "fec_carga": "17/09/2018",
+      "num_total": 500,
+      "num_pendientes": 200,
+      "num_pagados": 200,
+      "num_errores" : 100
+    },
+    {
+      "num_lote": 2,
+      "nom_archivo": "Banco Falabella",
+      "fec_carga": "16/09/2018",
+      "num_total": 1000,
+      "num_pendientes": 750,
+      "num_pagados": 100,
+      "num_errores": 150
+    },
+    {
+      "num_lote": 3,
+      "nom_archivo": "Ecopetrol",
+      "fec_carga": "15/09/2018",
+      "num_total": 500,
+      "num_pendientes": 200,
+      "num_pagados": 200,
+      "num_errores": 100
+    },
+      ];
 }
