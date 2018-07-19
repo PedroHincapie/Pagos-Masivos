a35bfc0a2bbbc0922f702117ed4151884c8616b5
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-07-19 14:36:00 -0500

    Add Componente Filter

--- pagos/src/app/app.module.ts
+++ pagos/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppFilter } from './app.filter';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFilter
+
   ],
   imports: [
     BrowserModule,
