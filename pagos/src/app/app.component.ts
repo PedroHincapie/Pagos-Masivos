da0eb3d847ad14d9e89a208eaa038c62005a22c7
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-07-23 07:45:30 -0500

    Adicion de Nuevos registros a la consulta

--- pagos/src/app/app.component.ts
+++ pagos/src/app/app.component.ts
@@ -36,5 +36,70 @@ export class AppComponent {
       "num_pagados": 200,
       "num_errores": 100
     },
-      ];
+    {
+      "num_lote": 4,
+      "nom_archivo": "Centro CC Los Molinos",
+      "fec_carga": "17/09/2018",
+      "num_total": 500,
+      "num_pendientes": 200,
+      "num_pagados": 200,
+      "num_errores": 100
+    },
+    {
+      "num_lote": 5,
+      "nom_archivo": "Banco Falabella",
+      "fec_carga": "16/09/2018",
+      "num_total": 1000,
+      "num_pendientes": 750,
+      "num_pagados": 100,
+      "num_errores": 150
+    },
+    {
+      "num_lote": 6,
+      "nom_archivo": "Ecopetrol",
+      "fec_carga": "15/09/2018",
+      "num_total": 500,
+      "num_pendientes": 200,
+      "num_pagados": 200,
+      "num_errores": 100
+    },
+    {
+      "num_lote": 7,
+      "nom_archivo": "Centro CC Los Molinos",
+      "fec_carga": "17/09/2018",
+      "num_total": 500,
+      "num_pendientes": 200,
+      "num_pagados": 200,
+      "num_errores": 100
+    },
+    {
+      "num_lote": 8,
+      "nom_archivo": "Banco Falabella",
+      "fec_carga": "16/09/2018",
+      "num_total": 1000,
+      "num_pendientes": 750,
+      "num_pagados": 100,
+      "num_errores": 150
+    },
+    {
+      "num_lote": 9,
+      "nom_archivo": "Ecopetrol",
+      "fec_carga": "15/09/2018",
+      "num_total": 500,
+      "num_pendientes": 200,
+      "num_pagados": 200,
+      "num_errores": 100
+    },
+    {
+      "num_lote": 10,
+      "nom_archivo": "Ecopetrol",
+      "fec_carga": "15/09/2018",
+      "num_total": 500,
+      "num_pendientes": 200,
+      "num_pagados": 200,
+      "num_errores": 100
+    },
+
+  ];
+
 }
